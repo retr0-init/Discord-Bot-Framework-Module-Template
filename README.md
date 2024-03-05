93aad94628f27e18bc07fda6442f4d2a7bd1038f
retr0-init <146389702+retr0-init@users.noreply.github.com>
2024-03-05 23:23:01 +0000

    More examples on local files, events and background tasks.

--- README.md
+++ README.md
@@ -8,10 +8,15 @@
 - 模块主要点在[`main.py`](main.py)中。
 - 所有主要的工作都应该在继承自`interactions.Extension`的类中进行。
 - 最好在指令基或指令组中定义你的命令。
+- 命令名不能：
+    - 是大写。
+    - 包含除下划线`_`之外的特殊符号。
+    - 包括空格。
 - 你可以用另一个`.py`文件来定义内部模块函数。这个文件需要与`main.py`同级。注意你要像以下来引入内部模块：
 ```python
 from . import xxx # xxx是内部模块的名字
 ```
+- 只能读写模块本地的文件。如果要读写本地文件，路径为`f"{os.path.dirname{__file__}/<文件名>"`.
 - 在[`requirements.txt`](requirements.txt)中写入模块所需的第三方库。
 - 在[`CHANGELOG`](CHANGELOG)中更新改动。
 - 在机器人内核文件夹以外的文件访问会被拒绝。
@@ -24,10 +29,15 @@ This is the module template for [Discord-Bot-Framework-Kernel](https://github.co
 - The module's entry point is in [`main.py`](main.py).
 - Do all the main work in the class inherited from `interactions.Extension`.
 - It's better to define your commands under either a command base or group.
+- The command name CANNOT be:
+    - Uppercase.
+    - Containing special characters other than underscore (`_`).
+    - Containing space.
 - You can use another `.py` file for internal module under the same directory as `main.py`. Be aware that you need to import it like
 ```python
 from . import xxx # xxx is the internal module script name
 ```
+- Only local files in the module directory can be read/written. THe path to the file is `f"{os.path.dirname{__file__}/<filename>` to interact with local files.
 - Put python module requirements in [`requirements.txt`](requirements.txt). Do NOT delete this file.
 - Update your changes in [`CHANGELOG`](CHANGELOG).
 - The file access to the files other than the kernel directory will be denied.
--- main.py
+++ main.py
@@ -20,6 +20,14 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import interactions
 # Use the following method to import the internal module in the current same directory
 from . import internal_t
+# Import the os module to get the parent path to the local files
+import os
+# aiofiles module is recommended for file operation
+import aiofiles
+# You can listen to the interactions.py event
+from interactions.api.events import MessageCreate
+# You can create a background task
+from interactions import Task, IntervalTrigger
 
 '''
 Replace the ModuleName with any name you'd like
@@ -53,5 +61,30 @@ class ModuleName(interactions.Extension):
         opt_type = interactions.OptionType.STRING
     )
     async def module_group_pong(self, ctx: interactions.SlashContext, option_name: str):
-        await ctx.send(f"Pong {option_name}!")
+        # The local file path is inside the directory of the module's main script file
+        async with aiofiles.open(f"{os.path.dirname(__file__)}/example_file.txt") as afp:
+            file_content: str = await afp.read()
+        await ctx.send(f"Pong {option_name}!\nFile content: {file_content}")
         internal_t.internal_t_testfunc()
+
+    @interactions.listen(MessageCreate)
+    async def on_messagecreate(self, event: MessageCreate):
+        '''
+        Event listener when a new message is created
+        '''
+        print(f"User {event.message.author.display_name} sent '{event.message.content}'")
+
+    # You can even create a background task to run as you wish.
+    # Refer to https://interactions-py.github.io/interactions.py/Guides/40%20Tasks/ for guides
+    # Refer to https://interactions-py.github.io/interactions.py/API%20Reference/API%20Reference/models/Internal/tasks/ for detailed APIs
+    @Task.create(IntervalTrigger(minutes=1))
+    async def task_everyminute(self):
+        channel: interactions.TYPE_MESSAGEABLE_CHANNEL = self.bot.get_guild(1234567890).get_channel(1234567890)
+        await channel.send("Background task send every one minute")
+        print("Background Task send every one minute")
+
+    # The command to start the task
+    @module_base.subcommand("start_task", sub_cmd_description="Start the background task")
+    async def module_base_starttask(self, ctx: interactions.SlashContext):
+        self.task_everyminute.start()
+        await ctx.send("Task started")
